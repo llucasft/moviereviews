d00a5c5dc7c7319fa6f84058367212e2db565987
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-04-28 14:21:13 -0300

    News app e renderizando dados no novo app

--- news/models.py
+++ news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.shortcuts import render
 
-# Create your models here.
+
+class News(models.Model):
+    headline = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateField()
