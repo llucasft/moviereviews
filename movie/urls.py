ddd637ebbb1c6e3bfc2f8f3e23098c2486bb4ea0
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-04-29 16:02:14 -0300

    Criação do formulário de reviews na pag do filme

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:movie_id>', views.detail, name='detail')
+    path('<int:movie_id>', views.detail, name='detail'),
+    path('<int:movie_id>/create', views.createreview, name='createreview'),
 ]
