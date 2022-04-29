dd2a724b07e2f1262be7050d147185d4a3d0071f
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-04-29 16:48:18 -0300

    Autorizando apenas usuarios logados

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,10 @@
+from multiprocessing.sharedctypes import Value
 from re import S
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie, Review
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 
 
@@ -34,6 +36,7 @@ def detail(request, movie_id):
     'reviews':reviews})
 
 
+@login_required
 def createreview(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     if request.method == 'GET':
@@ -52,3 +55,27 @@ def createreview(request, movie_id):
             return render(request, 'createreview.html',
             {'form':ReviewForm(),
             'error':'bad data passed in'})
+
+
+@login_required
+def updatereview(request, review_id):
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    if request.method == 'GET':
+        form = ReviewForm(instance=review)
+        return render(request, 'updatereview.html',
+        {'review':review,'form':form})
+    else:
+        try:
+            form = ReviewForm(request.POST,instance=review)
+            form.save()
+            return redirect('detail',review.movie.id)
+        except ValueError:
+            return render(request, 'updatereview.html',
+            {'review':review,'form':form,'error':'Bad data in form'})
+
+
+@login_required
+def deletereview(request, review_id):
+    review = get_object_or_404(Review,pk=review_id, user=request.user)
+    review.delete()
+    return redirect('detail', review.movie.id)
