d0646306f8170c05a675e31fb9ac91f8effcac21
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-04-29 15:35:10 -0300

    Funcionalidade de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import UserCreateForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
@@ -31,3 +32,19 @@ def signupaccount(request):
 def logoutaccount(request):
     logout(request)
     return redirect('home')
+
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html',
+        {'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'],
+        password=request.POST['password'])
+        if user is None:(request, 'loginaccount.html',
+            {'form':AuthenticationForm(),
+            'error':'username and password do not match'})
+        else:
+            login(request, user)
+            return redirect('home')
+
